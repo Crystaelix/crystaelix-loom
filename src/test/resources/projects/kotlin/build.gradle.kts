28c3011818514ee2b916e820bc40e27027dc9e96
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-02-10 02:37:20 +0800

    Change plugin id

--- src/test/resources/projects/kotlin/build.gradle.kts
+++ src/test/resources/projects/kotlin/build.gradle.kts
@@ -3,7 +3,7 @@ import java.util.Properties
 plugins {
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.serialization") version "1.7.22"
-    id("dev.architectury.loom")
+    id("com.crystaelix.loom")
 	`maven-publish`
 }
 
