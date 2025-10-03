246a55058e6248c65914e8ad13217416867d80af
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-10-04 02:40:22 +0800

    Don't add unprotect on legacy forge
    Add MCP annotations on legacy forge

--- src/main/java/net/fabricmc/loom/configuration/LoomConfigurations.java
+++ src/main/java/net/fabricmc/loom/configuration/LoomConfigurations.java
@@ -205,14 +205,22 @@ public abstract class LoomConfigurations implements Runnable {
 			extendsFrom(JavaPlugin.TEST_COMPILE_CLASSPATH_CONFIGURATION_NAME, Constants.Configurations.FORGE_EXTRA);
 			extendsFrom(JavaPlugin.TEST_RUNTIME_CLASSPATH_CONFIGURATION_NAME, Constants.Configurations.FORGE_EXTRA);
 
-			// Add Forge/NeoForge shared dev-time dependencies
-			getDependencies().add(Constants.Configurations.FORGE_EXTRA, LoomVersions.UNPROTECT.mavenNotation());
+			// Add shared dev-time dependencies
 			getDependencies().add(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME, LoomVersions.JAVAX_ANNOTATIONS.mavenNotation());
 
-			// Add Forge-only dev-time dependencies
+			// Add Forge/NeoForge dev-time dependencies
+			if (extension.isModernForgeLike()) {
+				getDependencies().add(Constants.Configurations.FORGE_EXTRA, LoomVersions.UNPROTECT.mavenNotation());
+			}
+
+			// Add Forge dev-time dependencies
 			if (extension.isForge()) {
 				getDependencies().add(Constants.Configurations.FORGE_EXTRA, LoomVersions.NAMING_SERVICE.mavenNotation());
 				getDependencies().add(Constants.Configurations.FORGE_EXTRA, LoomVersions.MIXIN_REMAPPER_SERVICE.mavenNotation());
+			}
+
+			// Add Forge/LegacyForge dev-time dependencies
+			if (extension.isForge() || extension.isLegacyForgeLike()) {
 				getDependencies().add(Constants.Configurations.FORGE_EXTRA, LoomVersions.MCP_ANNOTATIONS.mavenNotation());
 			}
 		}
