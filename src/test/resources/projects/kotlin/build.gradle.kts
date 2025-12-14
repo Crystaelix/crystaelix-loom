345e3974a729ec4a75f9476e0abedcb6e114deb9
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-12-15 03:18:55 +0800

    Fix tests attempt 1

--- src/test/resources/projects/kotlin/build.gradle.kts
+++ src/test/resources/projects/kotlin/build.gradle.kts
@@ -1,11 +1,10 @@
-import java.util.Properties
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	kotlin("jvm") version "2.0.21"
 	kotlin("plugin.serialization") version "2.0.21"
-    id("dev.architectury.loom")
+    id("com.crystaelix.loom")
 	`maven-publish`
 }
 
