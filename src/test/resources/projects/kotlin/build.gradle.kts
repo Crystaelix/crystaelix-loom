70ed1dcbd30198c8eec04508a30248cc359d6198
rafi67000 <kontakt@rafi67000.xyz>
2026-03-01 16:58:43 +0100

    Somehow it got lost while merging

--- src/test/resources/projects/kotlin/build.gradle.kts
+++ src/test/resources/projects/kotlin/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	kotlin("jvm") version "2.0.21"
 	kotlin("plugin.serialization") version "2.0.21"
-    id("com.crystaelix.loom")
+    id("xyz.vintageforge.loom")
 	`maven-publish`
 }
 
